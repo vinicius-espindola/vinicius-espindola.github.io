4b91e62b95b9905339f60d44fad5673faed30830
vinicius espindola <vinicius.espindola@rede.ulbra.br>
2018-11-29 06:34:22 -0200

    Alterações no meu site

---
+++ index.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+    $("#btntela").click(function(){
+        $("#modaltela").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btnatual").click(function(){
+        $("#modalatual").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btnacessorios").click(function(){
+        $("#modalacessorios").modal();
+    });
+});
